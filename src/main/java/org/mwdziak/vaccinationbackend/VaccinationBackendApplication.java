5554916b235419b7dac83ba5295061db0558989f
mwdziak <mwdziak@gmail.com>
2024-03-01 15:46:51 +0100

    Rebased master to main

---
+++ src/main/java/org/mwdziak/vaccinationbackend/VaccinationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mwdziak.vaccinationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mwdziak/vaccinationbackend/VaccinationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mwdziak.vaccinationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
