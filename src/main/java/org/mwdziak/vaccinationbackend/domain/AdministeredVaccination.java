efda4ddae4cd047d64118fca59d4267e82820766
mwdziak <mwdziak@gmail.com>
2024-03-01 16:27:10 +0100

    AdministeredVaccination entity class defined

---
+++ src/main/java/org/mwdziak/vaccinationbackend/domain/AdministeredVaccination.java
@@ -0,0 +1,23 @@
+package org.mwdziak.vaccinationbackend.domain;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+public class AdministeredVaccination {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Vaccination vaccination;
+    @Nonnull
+    private LocalDateTime date;
+}
