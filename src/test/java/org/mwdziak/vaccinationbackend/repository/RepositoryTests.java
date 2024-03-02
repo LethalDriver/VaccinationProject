cfa9990c0738af2420c6b2c1642a416496c5ddc3
mwdziak <mwdziak@gmail.com>
2024-03-02 10:42:33 +0100

    User repository integration test finished

---
+++ src/test/java/org/mwdziak/vaccinationbackend/repository/RepositoryTests.java
@@ -0,0 +1,30 @@
+package org.mwdziak.vaccinationbackend.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+abstract class RepositoryTests {
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0.26")
+            .withDatabaseName("vaccination")
+            .withUsername("vaccination")
+            .withPassword("vaccination");
+
+
+
+    @DynamicPropertySource
+    static void mySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+}
