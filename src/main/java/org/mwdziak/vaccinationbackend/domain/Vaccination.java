5a5a97cc8c657aa4e153ff7fa85cc0443f960c35
mwdziak <mwdziak@gmail.com>
2024-03-02 08:48:48 +0100

    Minor fixes

--- src/main/java/org/mwdziak/vaccinationbackend/domain/Vaccination.java
+++ src/main/java/org/mwdziak/vaccinationbackend/domain/Vaccination.java
@@ -4,11 +4,14 @@ import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Vaccination {
     @Id
@@ -16,5 +19,5 @@ public class Vaccination {
     private Long id;
     @Nonnull
     private String name;
-    private String recommendedAge;
+    private Integer recommendedAge;
 }
--- src/main/java/org/mwdziak/vaccinationbackend/repository/UserRepository.java
+++ src/main/java/org/mwdziak/vaccinationbackend/repository/UserRepository.java
@@ -2,9 +2,12 @@ package org.mwdziak.vaccinationbackend.repository;
 
 import org.mwdziak.vaccinationbackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
