f3ee0b3d5d1e876045c5ce83ad70f7a5302a0c80
mwdziak <mwdziak@gmail.com>
2024-03-03 13:22:25 +0100

    VaccinationRepository test implemented and passed

--- src/main/java/org/mwdziak/vaccinationbackend/domain/Vaccine.java
+++ src/main/java/org/mwdziak/vaccinationbackend/domain/Vaccine.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Vaccine {
     @Id
     @GeneratedValue
--- src/main/java/org/mwdziak/vaccinationbackend/repository/VaccineRepository.java
+++ src/main/java/org/mwdziak/vaccinationbackend/repository/VaccineRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
+    public Optional<Vaccine> findByName(String name);
     @Query("SELECT v FROM Vaccine v WHERE v.recommendedAge BETWEEN ?1 AND ?1 + 1")
     public List<Vaccine> findRecommendedVaccinesByAge(Integer age);
 }
