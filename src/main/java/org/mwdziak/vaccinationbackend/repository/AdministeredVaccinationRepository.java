7a3e9ef1449ba71a211d2508e4865ccc528f98b2
mwdziak <mwdziak@gmail.com>
2024-03-03 12:59:01 +0100

    Vaccination repository interface added

--- src/main/java/org/mwdziak/vaccinationbackend/repository/AdministeredVaccinationRepository.java
+++ src/main/java/org/mwdziak/vaccinationbackend/repository/AdministeredVaccinationRepository.java
@@ -3,5 +3,7 @@ package org.mwdziak.vaccinationbackend.repository;
 import org.mwdziak.vaccinationbackend.domain.AdministeredVaccination;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdministeredVaccinationRepository extends JpaRepository<AdministeredVaccination, Long> {
+import java.util.List;
+
+public interface AdministeredVaccinationRepository extends JpaRepository<AdministeredVaccination, Long>, VaccinationRepository {
 }
--- src/main/java/org/mwdziak/vaccinationbackend/repository/ScheduledVaccinationRepository.java
+++ src/main/java/org/mwdziak/vaccinationbackend/repository/ScheduledVaccinationRepository.java
@@ -3,5 +3,5 @@ package org.mwdziak.vaccinationbackend.repository;
 import org.mwdziak.vaccinationbackend.domain.ScheduledVaccination;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduledVaccinationRepository extends JpaRepository<ScheduledVaccination, Long> {
+public interface ScheduledVaccinationRepository extends JpaRepository<ScheduledVaccination, Long>, VaccinationRepository {
 }
