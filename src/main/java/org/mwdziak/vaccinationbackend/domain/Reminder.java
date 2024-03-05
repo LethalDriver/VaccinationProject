88cf870e19deb2bd96be10be169fe88e56f05337
mwdziak <mwdziak@gmail.com>
2024-03-05 13:46:06 +0100

    Reminder entity separated from the scheduled vaccination entity to further normalize the model.

---
+++ src/main/java/org/mwdziak/vaccinationbackend/domain/Reminder.java
@@ -0,0 +1,19 @@
+package org.mwdziak.vaccinationbackend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Reminder {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String dateTime;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private ScheduledVaccination scheduledVaccination;
+}
