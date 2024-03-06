ea215d24b5db4b5b6abb85643447fc891f72bfdd
mwdziak <mwdziak@gmail.com>
2024-03-06 17:13:54 +0100

    Updated vaccination mapper to set ScheduledVaccine to reminders

--- src/main/java/org/mwdziak/vaccinationbackend/mapper/ScheduledVaccinationMapper.java
+++ src/main/java/org/mwdziak/vaccinationbackend/mapper/ScheduledVaccinationMapper.java
@@ -1,10 +1,10 @@
 package org.mwdziak.vaccinationbackend.mapper;
 
-import org.mapstruct.Builder;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.*;
+import org.mwdziak.vaccinationbackend.domain.Reminder;
 import org.mwdziak.vaccinationbackend.domain.ScheduledVaccination;
+import org.mwdziak.vaccinationbackend.domain.Vaccination;
+import org.mwdziak.vaccinationbackend.domain.Vaccine;
 import org.mwdziak.vaccinationbackend.dto.ScheduledVaccinationDTO;
 
 import java.time.LocalDateTime;
@@ -19,4 +19,9 @@ public interface ScheduledVaccinationMapper extends DateParser {
     @Mapping(source = "dateTime", target = "dateTime", qualifiedByName = "stringToDateTime")
     @Mapping(source = "nextDoseDateTime", target = "nextDoseDateTime", qualifiedByName = "stringToDateTime")
     ScheduledVaccination toEntity(ScheduledVaccinationDTO scheduledVaccinationDTO);
+
+    @AfterMapping
+    default void setVaccination(@MappingTarget ScheduledVaccination scheduledVaccination) {
+        scheduledVaccination.getReminders().forEach(reminder -> reminder.setScheduledVaccination(scheduledVaccination));
+    }
 }
