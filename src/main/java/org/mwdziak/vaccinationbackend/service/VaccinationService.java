16aa6869fdd9f4955a5f7e85a22179755846d2d4
mwdziak <mwdziak@gmail.com>
2024-03-06 18:23:59 +0100

    Vaccination service now doesn't overwrite vaccines, but assigns already existing in the db vaccine to the scheduled and administered vaccination entities.

--- src/main/java/org/mwdziak/vaccinationbackend/service/VaccinationService.java
+++ src/main/java/org/mwdziak/vaccinationbackend/service/VaccinationService.java
@@ -2,15 +2,14 @@ package org.mwdziak.vaccinationbackend.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.mwdziak.vaccinationbackend.domain.AdministeredVaccination;
-import org.mwdziak.vaccinationbackend.domain.ScheduledVaccination;
-import org.mwdziak.vaccinationbackend.domain.User;
+import org.mwdziak.vaccinationbackend.domain.*;
 import org.mwdziak.vaccinationbackend.dto.AdministeredVaccinationDTO;
 import org.mwdziak.vaccinationbackend.dto.ScheduledVaccinationDTO;
 import org.mwdziak.vaccinationbackend.mapper.AdministeredVaccinationMapper;
 import org.mwdziak.vaccinationbackend.mapper.ScheduledVaccinationMapper;
 import org.mwdziak.vaccinationbackend.repository.AdministeredVaccinationRepository;
 import org.mwdziak.vaccinationbackend.repository.ScheduledVaccinationRepository;
+import org.mwdziak.vaccinationbackend.repository.VaccineRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,32 +20,36 @@ public class VaccinationService {
     private final UserService userService;
     private final ScheduledVaccinationRepository scheduledVaccinationRepository;
     private final AdministeredVaccinationRepository administeredVaccinationRepository;
-    public ScheduledVaccination scheduleVaccination(ScheduledVaccinationDTO scheduledVaccinationDTO) {
+    private final VaccineRepository vaccineRepository;
+    public void scheduleVaccination(ScheduledVaccinationDTO scheduledVaccinationDTO) {
         ScheduledVaccination scheduledVaccination = scheduledVaccinationMapper.toEntity(scheduledVaccinationDTO);
         User currentUser = userService.getCurrentUser();
         scheduledVaccination.setUser(currentUser);
-        return scheduledVaccinationRepository.save(scheduledVaccination);
+        findAndSetVaccine(scheduledVaccinationDTO.vaccineId(), scheduledVaccination);
+        scheduledVaccinationRepository.save(scheduledVaccination);
     }
     public void deleteScheduledVaccination(Long id) {
         scheduledVaccinationRepository.deleteById(id);
     }
-    public ScheduledVaccination editScheduledVaccination(ScheduledVaccinationDTO scheduledVaccinationDTO) {
+    public void editScheduledVaccination(ScheduledVaccinationDTO scheduledVaccinationDTO) {
         ScheduledVaccination updatedVaccination = scheduledVaccinationMapper.toEntity(scheduledVaccinationDTO);
 
         ScheduledVaccination existingVaccination = scheduledVaccinationRepository.findById(scheduledVaccinationDTO.id())
                 .orElseThrow(() -> new EntityNotFoundException("ScheduledVaccination not found"));
 
-        existingVaccination.setVaccine(updatedVaccination.getVaccine());
+
         existingVaccination.setDateTime(updatedVaccination.getDateTime());
         existingVaccination.setNextDoseDateTime(updatedVaccination.getNextDoseDateTime());
         existingVaccination.setReminders(updatedVaccination.getReminders());
+        findAndSetVaccine(scheduledVaccinationDTO.vaccineId(), existingVaccination);
 
-        return scheduledVaccinationRepository.save(existingVaccination);
+        scheduledVaccinationRepository.save(existingVaccination);
     }
     public AdministeredVaccination addAdministeredVaccination(AdministeredVaccinationDTO administeredVaccinationDTO) {
         AdministeredVaccination administeredVaccination = administeredVaccinationMapper.toEntity(administeredVaccinationDTO);
         User currentUser = userService.getCurrentUser();
         administeredVaccination.setUser(currentUser);
+        findAndSetVaccine(administeredVaccinationDTO.id(), administeredVaccination);
         return administeredVaccinationRepository.save(administeredVaccination);
     }
     public void deleteAdministeredVaccination(Long id) {
@@ -60,8 +63,14 @@ public class VaccinationService {
                 .orElseThrow(() -> new EntityNotFoundException("AdministeredVaccination not found"));
 
         existingVaccination.setDateTime(updatedVaccination.getDateTime());
-        existingVaccination.setVaccine(updatedVaccination.getVaccine());
+        findAndSetVaccine(administeredVaccinationDTO.id(), existingVaccination);
 
         return administeredVaccinationRepository.save(existingVaccination);
+    }
+
+    public <T extends Vaccination> void findAndSetVaccine(Long vaccineId, T vaccination) {
+        var vaccine = vaccineRepository.findById(vaccineId)
+                .orElseThrow(() -> new EntityNotFoundException("Vaccine not found"));
+        vaccination.setVaccine(vaccine);
     }
 }
