d769d9a1871329bd4c12ea5cb3d093176441ad50
mwdziak <mwdziak@gmail.com>
2024-03-08 11:22:22 +0100

    Added constructors and getters and setters to reminder message class

--- src/main/java/org/mwdziak/vaccinationbackend/dto/ReminderMessage.java
+++ src/main/java/org/mwdziak/vaccinationbackend/dto/ReminderMessage.java
@@ -1,5 +1,12 @@
 package org.mwdziak.vaccinationbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ReminderMessage {
     private Long id;
     private String dateTime;
