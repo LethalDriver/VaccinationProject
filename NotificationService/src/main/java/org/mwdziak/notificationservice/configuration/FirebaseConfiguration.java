a7f70bc40fa3dce549238cc3027d3a189d7fe2e7
mwdziak <mwdziak@gmail.com>
2024-03-10 20:49:06 +0100

    Moved firebase configuration logic to a dedicated configuration component

---
+++ NotificationService/src/main/java/org/mwdziak/notificationservice/configuration/FirebaseConfiguration.java
@@ -0,0 +1,38 @@
+package org.mwdziak.notificationservice.configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfiguration {
+
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+    @Bean
+    FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    @Bean
+    FirebaseApp firebaseApp(GoogleCredentials credentials) {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    GoogleCredentials googleCredentials() throws IOException {
+        return GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
+    }
+
+}
