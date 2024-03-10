661ca575065fab7dab6d264a86192be4d50c5f51
mwdziak <mwdziak@gmail.com>
2024-03-10 20:11:34 +0100

    Notification Service project setup

---
+++ NotificationService/src/main/java/org/mwdziak/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.mwdziak.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
---
+++ NotificationService/src/test/java/org/mwdziak/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mwdziak.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
