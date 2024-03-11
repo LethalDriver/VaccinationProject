99b0ea97efb062c27321e8a8a5dab92bb7e88ab6
mwdziak <mwdziak@gmail.com>
2024-03-11 15:42:59 +0100

    Kafka consumer updated to use the notification service to send notifications using firebase cloud messaging

--- NotificationService/src/main/java/org/mwdziak/notificationservice/service/KafkaConsumer.java
+++ NotificationService/src/main/java/org/mwdziak/notificationservice/service/KafkaConsumer.java
@@ -1,13 +1,16 @@
 package org.mwdziak.notificationservice.service;
 
+import lombok.RequiredArgsConstructor;
 import org.mwdziak.notificationservice.dto.ReminderMessage;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaConsumer {
+    private final NotificationSender notificationSender;
     @KafkaListener(topics = "vaccination-reminder")
     public void getNotificationsToSend(ReminderMessage reminderMessage){
-        //TODO
+        notificationSender.sendNotification(reminderMessage.getReminderTitle(), reminderMessage.getReminderMessage(), reminderMessage.getUsersToken());
     }
 }
