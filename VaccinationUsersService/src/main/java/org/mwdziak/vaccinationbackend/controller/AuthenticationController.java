3e4143e43df995dffd805874127202ee767918f8
mwdziak <mwdziak@gmail.com>
2024-03-31 17:37:42 +0200

    Refactored auth controller to specify the common path component on the class level.

--- VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/controller/AuthenticationController.java
+++ VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.mwdziak.vaccinationbackend.service.AuthenticationService;
 import org.mwdziak.vaccinationbackend.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.mwdziak.vaccinationbackend.dto.AuthenticationRequest;
 import org.mwdziak.vaccinationbackend.dto.AuthenticationResponse;
@@ -14,27 +15,28 @@ import org.mwdziak.vaccinationbackend.dto.TokensDTO;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     private final UserService userService;
 
-    @PostMapping("/auth/register")
+    @PostMapping("/register")
     public AuthenticationResponse registerUser(@RequestBody RegistrationRequest request) {
         userService.registerUser(request);
         return authenticationService.authenticate(new AuthenticationRequest(request.getEmail(), request.getPassword()));
     }
 
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public AuthenticationResponse loginUser(@RequestBody AuthenticationRequest user) {
         return authenticationService.authenticate(user);
     }
-    @PostMapping("/auth/refresh")
+    @PostMapping("/refresh")
     public AuthenticationResponse refreshToken(@RequestBody TokensDTO request) {
         return authenticationService.refresh(request);
     }
 
-    @PostMapping("/auth/logout")
+    @PostMapping("/logout")
     public void logout(@RequestBody TokensDTO request) {
         authenticationService.logout(request);
     }
