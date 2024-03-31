0c1b0fc86072ff7aee102a9ac980c4feb213da76
mwdziak <mwdziak@gmail.com>
2024-03-31 17:36:30 +0200

    Fixed request mapping in user controller.

--- VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/controller/UserController.java
+++ VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/controller/UserController.java
@@ -6,10 +6,12 @@ import org.mwdziak.vaccinationbackend.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
     @PostMapping("/notification-token")
