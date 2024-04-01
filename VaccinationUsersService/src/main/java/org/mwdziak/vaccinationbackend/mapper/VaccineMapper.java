4eb774d6abfd8cb599ae0bc2010312ab27da3ab4
mwdziak <mwdziak@gmail.com>
2024-04-01 15:18:56 +0200

    Updated vaccine mapper to use RemainingDoseMapper.

--- VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/mapper/VaccineMapper.java
+++ VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/mapper/VaccineMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mwdziak.vaccinationbackend.domain.Vaccine;
 import org.mwdziak.vaccinationbackend.dto.VaccineDTO;
 
-@Mapper(builder = @Builder(disableBuilder = true), componentModel = "spring")
+@Mapper(builder = @Builder(disableBuilder = true), componentModel = "spring", uses = {RemainingDoseMapper.class})
 public interface VaccineMapper {
     VaccineDTO toDto(Vaccine vaccine);
     Vaccine toEntity(VaccineDTO vaccineDTO);
