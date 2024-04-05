a7a2516f72877158e16092bc19c02a3f3c085f9c
mwdziak <mwdziak@gmail.com>
2024-04-05 10:42:01 +0200

    Renamed days before previous dose to days after previous dose in remaining dose

--- VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/dto/RemainingDoseDTO.java
+++ VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/dto/RemainingDoseDTO.java
@@ -3,6 +3,6 @@ package org.mwdziak.vaccinationbackend.dto;
 public record RemainingDoseDTO(
         Long id,
         Integer doseNumber,
-        Integer daysBeforePreviousDose
+        Integer daysAfterPreviousDose
 ) {
 }
