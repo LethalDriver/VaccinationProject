48ba4371d69d7f432b058a67b97b6d8ea7d36ad1
mwdziak <mwdziak@gmail.com>
2024-04-07 15:34:47 +0200

    Changed messaging implementation from kafka to sqs

--- VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/service/ReminderEmitter.java
+++ VaccinationUsersService/src/main/java/org/mwdziak/vaccinationbackend/service/ReminderEmitter.java
@@ -1,15 +1,18 @@
 package org.mwdziak.vaccinationbackend.service;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mwdziak.vaccinationbackend.dto.reminder.ReminderMessage;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReminderEmitter {
-    private final KafkaTemplate<String, ReminderMessage> kafkaTemplate;
+    private final SqsTemplate sqsTemplate;
     public void emitReminder(ReminderMessage reminder) {
-        kafkaTemplate.sendDefault(reminder);
+        var result = sqsTemplate.send("Reminders", reminder);
+        log.info(result.toString());
     }
 }
